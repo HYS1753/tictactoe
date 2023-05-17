5f64fe732c2312ae99143e3effa1267817313948
ShimHayong <hys1753@kyobobook.com>
2023-05-17 20:27:02 +0900

    feat(release): cross-env 패키지를 통한 개발 및 운영 환경 루트 디렉터리 이원화

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/tictactoe'
+  // gibhub 저장소 이름.
+  base: isProduction ? 'tictactoe' : '/'
 })
